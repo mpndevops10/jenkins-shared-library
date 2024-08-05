8652fb6dbc818f6cdc75909c3cf0fad06ccec062
Peter Ntsambi <125835309+mbohlip@users.noreply.github.com>
2024-08-05 04:26:17 -0700

    1st shared library (#2)
    * first shared-library file
    
    * 1st shared libary

---
+++ vars/etechApp.groovy
@@ -0,0 +1,24 @@
+def call(string repoUrl){
+  pipeline{
+    agent any
+    stages {
+      stage ("1-check") {
+        steps {
+          sh 'ls'
+          sh 'java --version'
+        }
+      }
+      stage ("checkout code"){
+        steps {
+          git branch: 'main'
+          url: "${repoUrl}"
+        }
+      }
+      stage ("other checkings"){
+        steps {
+          sh 'lsblk'
+        }
+      }
+    }
+  }
+}
